dbb228ffe4e15879582c31e418ef5b020d83fce9
HarishKumar93 <harishkumar.vanka@paradigmcreatives.com>
2014-09-09 16:50:31 +0530

    Completed

--- IndianSquad/gen/com/pcs/indiansquad/R.java
+++ IndianSquad/gen/com/pcs/indiansquad/R.java
@@ -11,10 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int flag=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int cricket=0x7f060001;
+        public static final int squad_btn=0x7f060000;
+    }
+    public static final class layout {
+        public static final int india=0x7f030000;
+        public static final int team=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int click=0x7f040001;
+        public static final int eigth=0x7f04000a;
+        public static final int eleven=0x7f04000d;
+        public static final int fifteen=0x7f040011;
+        public static final int fifth=0x7f040007;
+        public static final int first=0x7f040003;
+        public static final int fourteen=0x7f040010;
+        public static final int fourth=0x7f040006;
+        public static final int ninth=0x7f04000b;
+        public static final int second=0x7f040004;
+        public static final int seventh=0x7f040009;
+        public static final int sixteen=0x7f040012;
+        public static final int sixth=0x7f040008;
+        public static final int squad_btn=0x7f040002;
+        public static final int tenth=0x7f04000c;
+        public static final int third=0x7f040005;
+        public static final int thirteen=0x7f04000f;
+        public static final int twelve=0x7f04000e;
     }
     public static final class style {
         /** 
@@ -38,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
