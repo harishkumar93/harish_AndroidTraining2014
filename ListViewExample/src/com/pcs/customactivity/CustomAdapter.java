a1e64a49d423f03756cfa2e9adb9efa6c6cf970d
Harishkumar93 <harishkumar.vanka@paradigmcreatives.com>
2014-09-15 15:57:24 +0530

    View Holder and ConvertView are added to java code

--- ListViewExample/src/com/pcs/customactivity/CustomAdapter.java
+++ ListViewExample/src/com/pcs/customactivity/CustomAdapter.java
@@ -3,22 +3,27 @@ package com.pcs.customactivity;
 import java.util.ArrayList;
 
 import com.pcs.listviewexample.ListViewExample;
+import com.pcs.listviewexample.R;
 import com.pcs.playerslist.Player;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CustomAdapter  extends BaseAdapter{
 	private Context mcontext;
 	private ArrayList<Player> playerslist;
- 
+	private LayoutInflater layoutInflater;
+
 	public CustomAdapter(ListViewExample listViewExample,
 			ArrayList<Player> users) {
 		mcontext = listViewExample;
 		playerslist = users;
+		layoutInflater = LayoutInflater.from(mcontext);
 	}
 
 	@Override
@@ -31,7 +36,7 @@ public class CustomAdapter  extends BaseAdapter{
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
 		return playerslist.get(position);
-		
+
 	}
 
 	@Override
@@ -42,14 +47,62 @@ public class CustomAdapter  extends BaseAdapter{
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		TextView tv = new TextView(mcontext);
-		tv.setText("PlayerName : "+playerslist.get(position).getPlayerName()+ " \n" 
-				+"JerseyNumber:" +playerslist.get(position).getJerseyNumber() + "\n"
-				+"Country : "+playerslist.get(position).getCountry()+ " \n"
-				+"Stream: "+playerslist.get(position).getStream()+ " \n"
-			);
-		return tv;
+		Player players = (Player) getItem(position);
+		ViewHolder holder = null;
+
+		if(convertView==null)
+		{
+			convertView = layoutInflater.inflate(R.layout.details, null);
+			holder = new ViewHolder();
+			holder.imageView = (ImageView) convertView.findViewById(R.id.player_image);
+
+			holder.nameTxt = (TextView)convertView.findViewById(R.id.player_name);
+			holder.jerseyTxt = (TextView)convertView.findViewById(R.id.player_jersey);
+			holder.countryTxt = (TextView)convertView.findViewById(R.id.player_country);
+			holder.streamTxt = (TextView)convertView.findViewById(R.id.player_stream);
+		}
+		if(position==0)
+		{
+			holder.imageView.setBackgroundResource(R.drawable.dhoni);
+
+
+		}
+
+		else if(position==1)
+		{
+			holder.imageView.setBackgroundResource(R.drawable.sachin);
+
+
+		}
+		else if(position==2)
+		{
+			holder.imageView.setBackgroundResource(R.drawable.gilly);
+
+
+		}
+		else if(position==3)
+		{
+			holder.imageView.setBackgroundResource(R.drawable.mcgrath);
+
+
+		}
+
+		holder.nameTxt.setText("Player Name:-"+players.getPlayerName());
+		holder.jerseyTxt.setText("Jersey No:-"+players.getJerseyNumber());
+		holder.countryTxt.setText("Country:-"+players.getCountry());
+		holder.streamTxt.setText("Stream:-"+players.getStream());
+
+
+		return convertView;
+	}
+	public class ViewHolder
+	{
+		public ImageView imageView;
+		public TextView nameTxt;
+		public TextView jerseyTxt;
+		public TextView countryTxt;
+		public TextView streamTxt;
+
 	}
 
-	
 }
